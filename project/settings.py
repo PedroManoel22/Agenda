ca4dd34e0061ae6f40d7f47cc8f4194f74de6183
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-01 11:08:39 -0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [ # type: ignore
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'base_tamplates'],
+        'DIRS': [BASE_DIR / 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     BASE_DIR / 'base_static'
-)
+]
