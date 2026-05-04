cb9a1dcfec12d529629c6fdaba1cec6ff480f094
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-04 10:37:08 -0300

    Criando o primeiro model (Contact)

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# id (primary key)
+# first_name (str), last_name (str), phone (str)
+# email (email), created_date (date), description (text)
+# category (foreign key), show(boolean), owner (foreign key)
+# picture (imagem)
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True) # blank -> opcional
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True) # blank -> opcional
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True) # blank -> opcional
+
+
