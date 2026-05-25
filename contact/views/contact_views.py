25d7f61188fa90d3f7e1d259f3efd8f894fcf248
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-25 08:24:32 -0300

    Criando a view contact

---
+++ contact/views/contact_views.py
@@ -0,0 +1,9 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+
+
+def index(request: HttpRequest):
+    return render(
+        request,
+        "contact/index.html",
+    )
