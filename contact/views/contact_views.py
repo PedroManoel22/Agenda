4412d69adac0de9b401b9a74b726c4210a1d7b48
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-28 14:42:05 -0300

    Cria a view para exibir os detalhes de cada contato individualmente

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,11 +1,11 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from contact.models import Contact
 
 
 def index(request: HttpRequest):
-    contacts = Contact.objects.filter(show=True).order_by("-id")[0:2]
+    contacts = Contact.objects.filter(show=True).order_by("-id")[0:20]
 
     # print(contacts.query)
 
@@ -14,3 +14,16 @@ def index(request: HttpRequest):
     }
 
     return render(request, "contact/index.html", context)
+
+
+def contact(request: HttpRequest, contact_id: int):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    # print(contacts.query)
+
+    context = {
+        "contact": single_contact,
+    }
+
+    return render(request, "contact/contact.html", context)
