a245d0a1072c4fe9723a7bc9e20905fdc1087c04
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-28 14:43:30 -0300

    Adiciona rota para a página de detalhes do contato

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,10 @@
-
 from django.urls import path
+
 from contact import views
 
-app_name = 'contact'
+app_name = "contact"
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("<int:contact_id>/", views.contact, name="contact"),
+    path("", views.index, name="index"),
 ]
