b2302b9e985b592066a917efde19c77f21a5054a
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-28 10:26:58 -0300

    Adicionando Show

--- contact/admin.py
+++ contact/admin.py
@@ -3,20 +3,33 @@
 from django.contrib import admin
 from contact import models
 
-@admin.register(models.Contact) 
-class ContactAdmin(admin.ModelAdmin): # type: ignore
-    list_display = "id", "first_name","last_name", "phone",
-    ordering = "-id",
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):  # type: ignore
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "phone",
+        "show",
+    )
+    ordering = ("-id",)
     # list_filter = "created_date", # filtro por data de criação
-    search_fields = "id","first_name", "last_name", # campo de pesquisa
+    search_fields = (
+        "id",
+        "first_name",
+        "last_name",
+    )  # campo de pesquisa
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = "first_name", "last_name",
-    list_display_links = "id", "phone",
+    list_editable = "first_name", "last_name", "show"
+    list_display_links = (
+        "id",
+        "phone",
+    )
 
 
-@admin.register(models.Category) 
-class CategoryAdmin(admin.ModelAdmin): # type: ignore
-    list_display = "name",
-    ordering = "-id",
-    
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):  # type: ignore
+    list_display = ("name",)
+    ordering = ("-id",)
