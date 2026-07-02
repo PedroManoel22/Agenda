7470be7f56ba6b347de228077750c5ef2357d5a6
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-02 09:09:12 -0300

    Adiciona verificação de evio POST na view de criação

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 
 def create(request: HttpRequest):
+    if request.method == "POST":
+        print()
+        print(request.method)
+        print(request.POST.get("first_name"))
+        print(request.POST.get("last_name"))
+        print()
 
     context = {}
+
+    print()
+    print(request.method)
+    print()
 
     return render(request, "contact/create.html", context)  # type: ignore
