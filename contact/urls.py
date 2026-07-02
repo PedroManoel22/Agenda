4f0344e588e7b288ebe91767485429efae6e1913
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-02 08:21:56 -0300

    tirando alguns comentários

--- contact/urls.py
+++ contact/urls.py
@@ -8,13 +8,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
     # contact (CRUD)
-    path("contact/<int:contact_id>/detail/", views.contact, name="contact"),
-    # path("contact/create/", views.contact, name="contact"),
-    # path("contact/<int:contact_id>/update/", views.contact, name="contact"),
-    # path("contact/<int:contact_id>/delete/", views.contact, name="contact"),
-    # # users (CRUD)
-    # path("user/<int:contact_id>/detail/", views.contact, name="contact"),
-    # path("user/create/", views.contact, name="contact"),
-    # path("user/<int:contact_id>/update/", views.contact, name="contact"),
-    # path("user/<int:contact_id>/delete/", views.contact, name="contact"),
+    path("contact/<int:contact_id>/", views.contact, name="contact"),
+    path("contact/create/", views.create, name="create"),
 ]
