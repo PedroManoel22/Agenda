ce3cfe472e734d66670a379120e960a8aba677a2
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-28 14:34:54 -0300

    Configurando os coampos e widgets do formulário

--- contact/models.py
+++ contact/models.py
@@ -22,21 +22,20 @@ class Category(models.Model):
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50, blank=True) # blank -> opcional
+    last_name = models.CharField(max_length=50, blank=True)  # blank -> opcional
     phone = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254, blank=True) # blank -> opcional
+    email = models.EmailField(max_length=254, blank=True)  # blank -> opcional
     created_date = models.DateTimeField(default=timezone.now)
-    description = models.TextField(blank=True) # blank -> opcional
+    description = models.TextField(blank=True)  # blank -> opcional
     show = models.BooleanField(default=True)
-    picture = models.ImageField(blank=True, upload_to="pictures/%Y/%m/") # blank -> opcional
-    category = models.ForeignKey(Category, 
-                                 on_delete=models.SET_NULL,
-                                 blank=True, null=True) # -> quando eu apagar uma category esse campo fique null
-    
-    owner = models.ForeignKey(User, 
-                                 on_delete=models.SET_NULL,
-                                 blank=True, null=True)
-    
+    picture = models.ImageField(
+        blank=True, upload_to="pictures/%Y/%m/"
+    )  # blank -> opcional
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True
+    )  # -> quando eu apagar uma category esse campo fique null
+
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
