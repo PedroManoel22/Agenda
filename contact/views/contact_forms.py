be19cd13169a6b6ee9437795017064cd1e573a82
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-08-04 11:03:54 -0300

    implementa views create e update com tratamento de formulario e type hints

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,22 +1,61 @@
+from typing import Any
+
 from django.http import HttpRequest
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 
 from contact.forms import ContactForm
+from contact.models import Contact
 
 
 def create(request: HttpRequest):
 
+    form_action = reverse("contact:create")
+
     if request.method == "POST":
         form = ContactForm(request.POST)
 
-        context = {"form": form}
+        context: dict[str, Any] = {
+            "form": form,
+            "form_action": form_action,
+        }
 
         if form.is_valid():
-            form.save()
-            return redirect("contact:create")
+            contact: Contact = form.save()  # type: ignore
+            return redirect("contact:update", contact_id=contact.pk)  # type: ignore
+        return render(request, "contact/create.html", context)  # type: ignore
+
+    context = {
+        "form": ContactForm(),
+        "form_action": form_action,
+    }
+
+    return render(request, "contact/create.html", context)  # type: ignore
+
+
+def update(request: HttpRequest, contact_id: int):
+
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    form_action = reverse("contact:update", args=(contact_id,))
+
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
+
+        context: dict[str, Any] = {
+            "form": form,
+            "form_action": form_action,
+        }
+
+        if form.is_valid():
+            contact = form.save()  # type: ignore
+            return redirect("contact:update", contact_id=contact.pk)  # type: ignore
 
         return render(request, "contact/create.html", context)  # type: ignore
 
-    context = {"form": ContactForm()}
+    context = {
+        "form": ContactForm(instance=contact),
+        "form_action": form_action,
+    }
 
     return render(request, "contact/create.html", context)  # type: ignore
